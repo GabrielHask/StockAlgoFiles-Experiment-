dafe404bc751657b8d2db184c46541b1202a94b9
GabrielHask <gh5750@princeton.edu>
2025-03-26 12:45:24 -0400

    Update EarningStrat.py

--- EarningStrat.py
+++ EarningStrat.py
@@ -1,22 +1,26 @@
+# file for uploading daily stock data along with technical indicators using yfinance and TA
+
 import yfinance as yf
 import ta
 import pandas as pd
 import datetime as dt
 
-# Step 1: Fetch Apple stock data
+# Fetch Apple stock data
 ticker = "AAPL"
 data = yf.download(ticker, start="2023-05-01", end="2025-01-05", progress=False)
 
-# Step 2: Ensure all data columns are numeric
+# Ensure all data columns are numeric
 #data = data.apply(pd.to_numeric, errors="coerce")  # Convert non-numeric to NaN
 #data.dropna(inplace=True)  # Drop rows with NaN values
 
 
 
 #data = pd.read_excel("StockData4HR.xlsx")
+
+# Ensure data is in right format for calculating technical indicators
 close_series = data["Close"].squeeze()
 
-# Step 3: Calculate Technical Indicators using `ta`
+# Calculate Technical Indicators using `ta`
 data["SMA_2"] = ta.trend.sma_indicator(close_series, window=2)
 data["SMA_10"] = ta.trend.sma_indicator(close_series, window=10)
 data["EMA_2"] = ta.trend.ema_indicator(close_series, window=2)
@@ -33,7 +37,7 @@ data["RSI"] = ta.momentum.rsi(close_series, window=14)
 # VWAP (Volume Weighted Average Price)
 # data["VWAP"] = ta.volume.VolumeWeightedAveragePrice(high=data["High"].squeeze(), low=data["Low"].squeeze(), close=data["Close"].squeeze(), volume=data["Volume"].squeeze()).volume_weighted_average_price()
 
-# Step 4: Save to Excel
+# Save to Excel
 output_file = "StockDataAAPL.xlsx"
 
 data.to_excel(output_file, index=True)
